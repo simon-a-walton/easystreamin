cbbb73b8f11f9103f487fae848fb8cb33e6b262b
Simon Walton <saw29@hotmail.co.uk>
2020-11-12 18:58:28 +0000

    migration for playlists

---
+++ db/migrate/20201112164654_create_playlists.rb
@@ -0,0 +1,12 @@
+class CreatePlaylists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :playlists do |t|
+      t.string :name
+      t.string :listtype
+      t.string :tag
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
